33663b2ee18cf2826041db31d07253fbca010c4c
LacusFaustus <122123740+LacusFaustus@users.noreply.github.com>
2025-07-21 17:52:14 +0300

    Fix code style: add empty lines in HistoryManager and Main.java

--- src/main/java/ru/yandex/javacourse/Main.java
+++ src/main/java/ru/yandex/javacourse/Main.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         TaskManager taskManager = Managers.getDefault();
+
         Managers.getDefaultHistory();
 
         // Создаем задачи
