4952aaebfcb1b512e410e6e1c105808d1edf9506
LacusFaustus <122123740+LacusFaustus@users.noreply.github.com>
2025-07-25 19:46:10 +0300

    Исправление стиля кода в InMemoryTaskManagerTest: перенос фигурных скобок

--- src/test/java/ru/yandex/javacourse/service/InMemoryTaskManagerTest.java
+++ src/test/java/ru/yandex/javacourse/service/InMemoryTaskManagerTest.java
@@ -1,28 +1,46 @@
-package test.java.ru.yandex.javacourse.service;
+package ru.yandex.javacourse.service;
 
-import main.java.ru.yandex.javacourse.model.*;
-import main.java.ru.yandex.javacourse.service.*;
-import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class InMemoryTaskManagerTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ru.yandex.javacourse.model.Epic;
+import ru.yandex.javacourse.model.Subtask;
+import ru.yandex.javacourse.model.Task;
+
+class InMemoryTaskManagerTest
+{
     private TaskManager manager;
     private Task task;
     private Subtask subtask;
 
     @BeforeEach
-    void setUp() {
+    void setUp()
+    {
         manager = new InMemoryTaskManager();
         task = new Task("Test task", "Test description");
         Epic epic = new Epic("Test epic", "Test description");
-        int epicId = manager.createEpic(epic);
-        subtask = new Subtask("Test subtask", "Test description", epicId);
+        manager.createEpic(epic);
+        subtask = new Subtask("Test subtask", "Test description", epic.getId());
     }
 
     @Test
-    void shouldCreateAndRetrieveTask() {
+    @DisplayName("Should create and return task with same ID")
+    void shouldCreateAndRetrieveTaskWithSameId()
+    {
         int taskId = manager.createTask(task);
-        Task saved = manager.getTaskById(taskId);
-        assertEquals(taskId, saved.getId());
+        Task retrieved = manager.getTaskById(taskId);
+        assertEquals(taskId, retrieved.getId());
+    }
+
+    @Test
+    @DisplayName("Should create and return subtask")
+    void shouldCreateAndRetrieveSubtask()
+    {
+        int subtaskId = manager.createSubtask(subtask);
+        Subtask retrieved = manager.getSubtaskById(subtaskId);
+        assertEquals(subtaskId, retrieved.getId());
     }
 }
